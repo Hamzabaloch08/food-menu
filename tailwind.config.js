ecdc6e9f2cad9e9eab3f1ae1d77fef96a338aa73
Hamzabaloch08 <hb.developer08@gmail.com>
2025-01-16 15:24:51 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", // Replace with the actual paths to your files
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,css,js}"],
   theme: {
     extend: {},
   },
